4e4343ecd6b874f9a0df6fb86fe44b7e71c1c07e
caleqw2 <caleqw2@illinois.edu>
2021-03-26 00:08:49 -0500

    Updated front-end design with a new prototype

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import React from 'react'
 import './App.css';
 import CourseCard from './CourseCard';
 
 function App() {
+
   return (
     <div className="App">
-      <CourseCard />
+
+      {/* Header */}
+      <div className="header">
+        <h1>Course LMS Manager</h1>
+      </div>
+
+      <div className="cardFlexContainer">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+      </div> 
     </div>
   );
 }
--- src/CourseCard.js
+++ src/CourseCard.js
@@ -1,60 +1,59 @@
-import React from 'react';
-import "./CourseCard.css";
-import { Container, Card, CardColumns, Button, Row, Col } from "react-bootstrap";
+import React from 'react'
+import './App.css';
 
-//// TODO: Convert this to a JSON file and find a way to parse JSON -> Easier to
-//// communicate with
-// FIXME: contenteditable is not really practical. Can we change change and save
-// the text in front-end?
-const cardInfo = [
-  { courseName: "CS126", description: "Busy"},
-  { courseName: "CS173", description: "Chill"},
-  { courseName: "MATH241", description: "Sed"},
-  { courseName: "MUS130", description: "Fun"}
-]
-
-// Card border can be customized using CSS.
-// Work on CSS (.customCard), then add className="customCard" below
-// Reference: https://react-bootstrap.github.io/getting-started/introduction/
-// TODO: If you haven't, do npm install react-bootstrap bootstrap to run this!
-
-const renderCard = (card, index) => {
-  return (
-    <Card className='card' border='dark' style={{ width: '16rem' }} key={index}>
-      <Card.Body>
-        <Card.Title as="h3" contenteditable="true"> {card.courseName} </Card.Title>
-        <Card.Text>
-          <Row>
-            <Col xs="8">
-              <Row> Course Home </Row>
-              <Row> CampusWire </Row>
-              <Row> PrairieLearn </Row>
-            </Col>
-            <Col> Zoom </Col>
-          </Row>
-          <hr/>
-          <Row contenteditable="true">
-            {card.description} (Editable)
-          </Row>
-        </Card.Text>
-        <Button variant="light">Expand</Button>
-      </Card.Body>
-    </Card>
-  );
-};
-
+// TODO: Make everything modular (pass info to component by props rather than hard-coding everything)
 class CourseCard extends React.Component {
-  render() {
-    return (
-      <div className="grid">
-        <Container>
-          <CardColumns>
-          {cardInfo.map(renderCard)}
-          </CardColumns>
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="courseCard">
+                <h2>Course Title</h2>
+                <hr/>
+
+                <div className="linksWrapper">
+                    <div className="linksGroup">
+                        <LinkRow/>
+                        <hr/>
+                        <LinkRow/>
+                        <hr/>
+                        <LinkRow/>
+                    </div>
+                    <FeaturedLink/>
+                </div>
+                
+                <div className="infoText">
+                    <ul>
+                        <li>Here's one sample TODO item</li>
+                        <li>Here's another TODO item</li>
+                        <li>And a third!</li>
+                    </ul>
+                </div>
+                <img id="chevron" src="resources/chevron.png" alt="Chevron"/>
+            </div>
+        );
+    }
+}
+
+class FeaturedLink extends React.Component {
+    render() {
+        return (
+            <div className="featuredLink">
+                <h3>Zoom</h3>
+                <img src="resources/zoom.png" alt="zoom"/>
+                <p>MWF 3:00</p>
+            </div>
+        );
+    }
+}
+
+class LinkRow extends React.Component {
+    render() {
+        return (
+            <div className="linkRow">
+                <img src="resources/favicon.png" alt="logo"/>
+                <a href="youtube.com"><p>Campuswire</p></a>
+            </div>
+        );
+    }
 }
 
 export default CourseCard;
