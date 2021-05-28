d248c7955eeb8e71a3e46fa2010064a4355b5bd1
phantuanminh <phantuanminh2002@gmail.com>
2021-05-28 14:11:51 +0700

    Finish Dummy Sign In / Sign Up FullStack

--- backend/setup.py
+++ backend/setup.py
@@ -6,13 +6,13 @@ from flask_pymongo import PyMongo
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+app.secret_key = 'mysecret'
 
 # Enable Cross-Origin Requests
 CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['MONGO_DBNAME'] = 'course-management-hub'
-app.config['MONGO_URI'] = 'mongodb://admin:password@localhost:27017/?authSource=admin'
+app.config['MONGO_URI'] = 'mongodb://admin:password@mongodb:27017/course-management-hub?authSource=admin'
 
 mongo = PyMongo(app)
 
@@ -20,16 +20,14 @@ mongo = PyMongo(app)
 @cross_origin(supports_credentials=True)
 @app.route('/api/sign_in', methods=['POST'])
 def sign_in():
-    # data = request.get_json()
-    # return jsonify({'email': data['email'], 'password': data['password']})
-    # data = request.get_json()
-    # users = mongo.db.users
-    # sign_in_user = users.find_one({'email': data['email']})
+    data = request.get_json()
+    users = mongo.db.users
+    sign_in_user = users.find_one({'email': data['email']})
 
-    # if sign_in_user:
-    #     if bcrypt.hashpw(data['password'].encode('utf-8'), sign_in_user['password'].encode('utf-8')) == sign_in_user['password'].encode('utf-8'):
-    #         session['email'] = data['email']
-    #         return jsonify({'result': 'success'})
+    if sign_in_user:
+        if bcrypt.hashpw(data['password'].encode('utf-8'), sign_in_user['password'].encode('utf-8')) == sign_in_user['password'].encode('utf-8'):
+            session['email'] = data['email']
+            return jsonify({'result': 'success'})
 
     return jsonify({'result': 'fail'})
 
@@ -37,19 +35,17 @@ def sign_in():
 @app.route('/api/sign_up', methods=['POST'])
 @cross_origin(supports_credentials=True)
 def sign_up():
-    # data = request.get_json()
-    # return jsonify({'email': data['email'], 'password': data['password']})
-    # data = request.get_json()
-    # if request.method == 'POST':
-    #     users = mongo.db.users
-    #     existing_user = users.find_one({'email': data['email']})
-    #     if existing_user is None:
-    #         hashpass = bcrypt.hashpw(
-    #             data['password'].encode('utf-8'), bcrypt.gensalt())
-    #         users.insert(
-    #             {'email': data['email'], 'password': hashpass, 'firstName': data['firstName'], 'lastName': data['lastName']})
-    #         session['email'] = data['email']
-    #         return jsonify({'result': 'success'})
+    data = request.get_json()
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'email': data['email']})
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(
+                data['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert(
+                {'email': data['email'], 'password': hashpass, 'firstName': data['firstName'], 'lastName': data['lastName']})
+            session['email'] = data['email']
+            return jsonify({'result': 'success'})
 
     return jsonify({'result': 'fail'})
 
